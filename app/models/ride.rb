2ffcb600171c32d041ffb06db9a7009a7b25a712
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-02-24 16:45:26 +0530

    add mappable to model and associations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class Ride < ApplicationRecord
+  acts_as_mappable default_units: :kms,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :destination_lat,
+                   lng_column_name: :destination_lng
   belongs_to :user
   belongs_to :cab
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  acts_as_mappable default_units: :kms,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :lat,
+                   lng_column_name: :lng
+  has_many :rides
 end
